b11917ca149afa2b12d4058cf957a68ea9bc15c2
kytoaq <kytoaq@gmail.com>
2021-02-15 18:09:34 -0600

    trinket-common: Remove framework_compatibility_matrix
    By having _matrix compatibility the framework_compatibility_matrix file
    becomes a duplicate, which causes a conflict in both files.
    
    Follow error:
    Error: The following instances are in the device manifest but not specified in framework compatibility matrix:
        com.qualcomm.qti.dpm.api@1.0::IdpmQmi/dpmQmiService
        motorola.hardware.audio.adspd@1.0::IADSP/default
        vendor.qti.hardware.radio.qtiradio@2.4::IQtiRadio/slot1
        vendor.qti.hardware.radio.qtiradio@2.4::IQtiRadio/slot2
    Suggested fix:
    1. Check for any typos in device manifest or framework compatibility matrices with FCM version >= 4.
    2. Add them to any framework compatibility matrix with FCM version >= 4 where applicable.
    3. Add them to DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE or DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE.
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,8 +184,6 @@ USE_DEVICE_SPECIFIC_GPS := true
 # HIDL
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(COMMON_PATH)/framework_compatibility_matrix.xml
-
 
 # Kernel cmdline
 BOARD_KERNEL_CMDLINE := \
