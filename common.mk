1bc6691e6f8692e3546eaf2105225b906cc09982
kytoaq <kytoaq@gmail.com>
2021-04-07 05:13:56 -0500

    squash RIL 2 Erfan autor

--- common.mk
+++ common.mk
@@ -139,6 +139,19 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict
 PRODUCT_PRODUCT_VNDK_VERSION := current
