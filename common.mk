b4a4035532262420cf1cba7f70aeccf6e389d892
kytoaq <kytoaq@gmail.com>
2021-04-07 06:23:04 -0500

    trinket-common: Add debuggable updater
    * for update_engine_client
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- common.mk
+++ common.mk
@@ -51,6 +51,9 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client    
     
 # Additional native libraries
 PRODUCT_COPY_FILES += \
