d4c90c9af582314594cabaf67958550ffde09278
Deivid21 <david.parra.ignacio@gmail.com>
2021-07-21 01:48:25 -0400

    sm6125-common: Update Properties
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- properties.mk
+++ properties.mk
@@ -14,30 +14,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.audio.fluence.speaker=false \
     ro.vendor.audio.sdk.fluencetype=fluence \
     ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.adm.buffering.ms=2 \
-    vendor.audio.cts.media=false \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
-    vendor.audio.enable.mirrorlink=false \
-    vendor.audio.flac.sw.decoder.24bit=true \
-    vendor.audio.hal.output.suspend.supported=true \
-    vendor.audio.hw.aac.encoder=true \
-    vendor.audio.noisy.broadcast.delay=600 \
-    vendor.audio.offload.buffer.size.kb=32 \
-    vendor.audio.offload.gapless.enabled=true \
-    vendor.audio.offload.multiaac.enable=true \
-    vendor.audio.offload.multiple.enabled=false \
-    vendor.audio.offload.passthrough=false \
-    vendor.audio.offload.pstimeout.secs=3 \
-    vendor.audio.offload.track.enable=true \
-    vendor.audio.parser.ip.buffer.size=0 \
-    vendor.audio.safx.pbe.enabled=true \
-    vendor.audio.tunnel.encode=false \
-    vendor.audio.use.sw.alac.decoder=true \
-    vendor.audio.use.sw.ape.decoder=true \
-    vendor.audio_hal.in_period_size=144 \
-    vendor.audio_hal.period_multiplier=3 \
-    vendor.audio_hal.period_size=192 \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.use.sw.alac.decoder=false \
+    vendor.audio.use.sw.ape.decoder=false \
+    vendor.audio_hal.period_size=240 \
     vendor.voice.path.for.pcm.voip=false
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
@@ -47,50 +29,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     audio.offload.min.duration.secs=60 \
     persist.vendor.audio.hac.enable=false \
     ro.audio.monitorRotation=true
-
-PRODUCT_ODM_PROPERTIES += \
-    vendor.audio.feature.a2dp_offload.enable=true \
-    vendor.audio.feature.afe_proxy.enable=true \
-    vendor.audio.feature.anc_headset.enable=true \
-    vendor.audio.feature.audiozoom.enable=false \
-    vendor.audio.feature.battery_listener.enable=true \
-    vendor.audio.feature.compr_cap.enable=false \
-    vendor.audio.feature.compr_voip.enable=true \
-    vendor.audio.feature.compress_in.enable=true \
-    vendor.audio.feature.compress_meta_data.enable=true \
-    vendor.audio.feature.concurrent_capture.enable=true \
-    vendor.audio.feature.custom_stereo.enable=true \
-    vendor.audio.feature.deepbuffer_as_primary.enable=false \
-    vendor.audio.feature.display_port.enable=true \
-    vendor.audio.feature.dsm_feedback.enable=false \
-    vendor.audio.feature.dynamic_ecns.enable=true \
-    vendor.audio.feature.ext_hw_plugin.enable=true \
-    vendor.audio.feature.external_dsp.enable=false \
-    vendor.audio.feature.external_speaker.enable=false \
-    vendor.audio.feature.external_speaker_tfa.enable=false \
-    vendor.audio.feature.fluence.enable=true \
-    vendor.audio.feature.fm.enable=true \
-    vendor.audio.feature.hdmi_edid.enable=true \
-    vendor.audio.feature.hdmi_passthrough.enable=true \
-    vendor.audio.feature.hfp.enable=true \
-    vendor.audio.feature.hifi_audio.enable=false \
-    vendor.audio.feature.hwdep_cal.enable=false \
-    vendor.audio.feature.incall_music.enable=true \
-    vendor.audio.feature.keep_alive.enable=true \
-    vendor.audio.feature.kpi_optimize.enable=true \
-    vendor.audio.feature.maxx_audio.enable=false \
-    vendor.audio.feature.multi_voice_session.enable=true \
-    vendor.audio.feature.ras.enable=true \
-    vendor.audio.feature.record_play_concurency.enable=false \
-    vendor.audio.feature.snd_mon.enable=true \
-    vendor.audio.feature.spkr_prot.enable=false \
-    vendor.audio.feature.src_trkn.enable=true \
-    vendor.audio.feature.ssrec.enable=true \
-    vendor.audio.feature.usb_offload.enable=true \
-    vendor.audio.feature.usb_offload_burst_mode.enable=true \
-    vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
-    vendor.audio.feature.vbat.enable=true \
-    vendor.audio.feature.wsa.enable=false
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -104,10 +42,6 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.bluetooth.library_name=libbluetooth_qti.so \
     vendor.bluetooth.soc=cherokee
 
-# Camera
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.vendor.camera.expose.aux=1
-
 # CNE
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.vendor.cne.feature=1
@@ -119,6 +53,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 # Crypto
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.crypto.allow_encrypt_override=true \
+    ro.crypto.dm_default_key.options_format.version=2 \
     ro.crypto.volume.filenames_mode=aes-256-cts
 
 # Dalvik
@@ -172,7 +107,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.sf.force_brightness_capability=1 \
     debug.sf.enable_gl_backpressure=1 \
     ro.opengles.version=196610 \
-    ro.gfx.driver.1=com.qualcomm.qti.gpudrivers.trinket.api30 \
     vendor.display.enable_default_color_mode=1 \
     vendor.gralloc.disable_ubwc=0
 
@@ -189,6 +123,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Incremental FS
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.incremental.enable=1
+
+# Keymaster 4.0 - TDES support
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore_desede=true
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -214,7 +152,7 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 # Netflix
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.netflix.bsp_rev=Q6150-17263-1
+    ro.netflix.bsp_rev=Q6125-17995-1
 
 # NFC
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
