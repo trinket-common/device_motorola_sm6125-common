254773fee2b46d5dc1a5b6d5198b896e9cde23bd
Deivid21 <david.parra.ignacio@gmail.com>
2021-07-20 21:45:15 -0400

    sm6125-common: Move audio_effects to devices
    Signed-off-by: Deivid21 <david.parra.ignacio@gmail.com>

--- common.mk
+++ common.mk
@@ -138,7 +138,6 @@ PRODUCT_PACKAGES += \
     libtinycompress.vendor
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml
 
 PRODUCT_COPY_FILES += \
